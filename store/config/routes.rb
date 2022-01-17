a86d4259233b80e121527ce1eef2840454dae8b7
JDMPasquel <jdamupasquel@unicauca.edu.co>
2022-01-17 10:34:18 -0500

    CRUD - Ruby On Rails

---
+++ store/config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get '/products', to: 'products#index'
+  get '/products/new', to: 'products#new'
+  post '/products', to: 'products#create'
+  get '/product/:id', to: 'products#show', as: 'product'
+  get '/products/:id/edit', to: 'products#edit', as: 'edit_product'
+  patch '/product/:id', to: 'products#update'
+  put '/product/:id', to: 'products#update'
+  delete '/product/:id', to: 'products#delete'
+end
